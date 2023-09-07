4d345733218c8697bf270d893010e23d3d0476e4
kess4obehi <kessiena4excellence@gmail.com>
2023-09-07 14:16:41 -0700

    Program that returns standard error

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,13 @@
 #include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - a c program that prints a line to the standard error
- * Return: 1 (successful)
+ *main - Entry point
+ *
+ *Return: Always 0 (Success)
  */
 int main(void)
 {
-	fputs("and that piece of art is useful" - Dora Korpar, 2015-10-19\n", stdout);
-	return (1);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+			return (1);
 }
