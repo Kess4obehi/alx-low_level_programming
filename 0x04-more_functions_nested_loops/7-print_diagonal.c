3113c126e4321373de4a304868335ee6fce24bbd
kess4obehi <kessiena4excellence@gmail.com>
2023-09-14 14:07:21 -0700

    I feel like I am diagonally parked in a parallel universe

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * print_diagonal - function that draws a diagonal line
+ * @n: letter to be computed
+ * Return: 0
+ */
+
+void print_diagonal(int n)
+{
+	int i, j;
+
+	if (n <= 0)
+		_putchar('\n');
+	else
+	{
+		for (i = 1; i < n; i++)
+		{
+			for (j = 1; j < n; j++)
+			{
+				if (j == i)
+					_putchar('\\');
+				else if (j < i)
+					_putchar(' ');
+			}
+			_putchar('\n');
+		}
+	}
+}
