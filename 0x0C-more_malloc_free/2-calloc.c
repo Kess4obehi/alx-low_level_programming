4bf934edfdc3a324068acf395b1ff274b7a3e7f2
kess4obehi <kessiena4excellence@gmail.com>
2023-10-08 12:37:27 -0700

    string_nconcat

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,36 @@
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * *_elementset - fills memory with a constant byte
+ * @p: 
+
+char *_elementset(char *p, char c, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+		p[i] = c;
+	return (p);
+}
+
+/**
+ * _calloc - A function that allocates memory for an array, using malloc.
+ * @nmemb: elements of array
+ * @size: bytes of memory to be allocated
+ * Return: 0
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *ptr;
+
+	ptr = malloc(nmemb * size);
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+	if (ptr == NULL)
+		return (NULL);
+	_elementset(ptr, 0, nmemb * size);
+	return (ptr);
+}
