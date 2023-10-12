684e5114823167b923ab3812137ca88e17968c8f
kess4obehi <kessiena4excellence@gmail.com>
2023-10-12 12:19:15 -0700

    To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,52 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * print_all - A function that prints anything.
+ * @format: list of types of arguments passed to the function
+ * Return: 0
+ */
+
+void print_all(const char * const format, ...)
+{
+	unsigned int i;
+	va_list list;
+
+	char *str, *sep;
+
+	va_start(list, format);
+
+	sep = "";
+
+	i = 0;
+	while (format && format[i])
+	{
+		switch (format[i])
+		{
+			case 'c':
+				printf("%s%c", sep, va_arg(list, int));
+				break;
+			case 'i':
+				printf("%s%d", sep, va_arg(list, int));
+				break;
+			case 'f':
+				printf("%s%f", sep, va_arg(list, double));
+				break;
+			case 's':
+				str = va_arg(list, char *);
+				if (str == NULL)
+					printf("(nil)");
+				printf("%s%s", sep, str);
+				break;
+			default:
+				i++;
+				continue;
+		}
+		sep = ", ";
+		i++;
+	}
+	va_end(list);
+	printf("\n");
+}
