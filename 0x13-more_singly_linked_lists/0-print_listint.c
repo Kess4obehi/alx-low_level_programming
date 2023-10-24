fa2fdc967ea2a975cc91eb5a3f7607f929852220
kess4obehi <kessiena4excellence@gmail.com>
2023-10-24 15:37:50 -0700

    Print list

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -2,7 +2,7 @@
 
 /**
  * print_listint - A function that prints all the elements of a listint_t list.
- * @h: linked list of type listint_t to print
+ * @h: pointer to the head
  *
  * Return: number of nodes
  */
