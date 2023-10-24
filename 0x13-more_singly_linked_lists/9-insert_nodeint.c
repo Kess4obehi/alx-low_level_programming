a66e78502fc78044a5f82ef52eb7e5019bff3cff
kess4obehi <kessiena4excellence@gmail.com>
2023-10-24 10:12:39 -0700

    Insert

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -17,7 +17,7 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	newnode = malloc(sizeof(listint_t));
 
-	if (!newnode && !*head)
+	if (!newnode && !head)
 		return (NULL);
 
 	newnode->n = n;
@@ -25,8 +25,9 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	if (idx == 0)
 	{
-		newnode->next = *head;
+		newnode->next = temp;
 		*head = newnode;
+		return (newnode);
 	}
 	for (i = 0; temp && (i < idx); i++)
 	{
