09ba72810d086db09cd121e075d3b1a1986f53be
kess4obehi <kessiena4excellence@gmail.com>
2024-07-18 20:58:28 -0700

    corrections made

--- 0x1E-search_algorithms/100-jump.c
+++ 0x1E-search_algorithms/100-jump.c
@@ -7,16 +7,17 @@
  * @size: number of elements in array
  * @value: the value to search for
  * Return: If the value is not present or the array is null, -1.
- * 	   Otherwise, the first index where the value is located.
+ * Otherwise, the first index where the value is located.
  *
  * Description: Prints a value every time it is compared in the array.
- * 		Uses the square root of the arrray size as the jump step.
+ * Uses the square root of the arrray size as the jump step.
  *
  */
 
 int jump_search(int *array, size_t size, int value)
 {
 	size_t i, jump, step;
+
 	step = sqrt(size);
 
 	for (i = jump = 0; jump < size;)
@@ -27,7 +28,7 @@ int jump_search(int *array, size_t size, int value)
 	}
 	printf("Value found between indexes [%ld] = [%ld]", i, jump);
 
-	jump = jump < size -1 ? jump : size - 1;
+	jump = jump < size - 1 ? jump : size - 1;
 	for (; i < jump && array[i] < value; i++)
 		printf("Value checked array[%ld] = [%d]\n" i, array[i]);
 	printf("Value checked array[%ld] = [%d]\n", i, array[i]);
